7fabcdca0683310c47ac251ae991bd4278b5bac3
Jose Elera <jelera@gmail.com>
2019-03-29 17:03:13 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,8 +1,11 @@
+require "pry"
+
 class Hash
   def keys_of(*arguments)
     # code goes here
     output = []
 
+    binding.pry
     self.each do |k,v|
       output << k if arguments.include?(v)
     end
@@ -10,3 +13,5 @@ class Hash
     output
   end
 end
+
+jose = Hash.new.keys_of("iker", "casillas")
