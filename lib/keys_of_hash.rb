3fee5ff7d9dc75053a1c3e9a89d022ef662517df
Jose Elera <jelera@gmail.com>
2019-03-29 16:59:32 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,13 @@
 class Hash
   def keys_of(arguments)
     # code goes here
+    output = []
+    arguments.each do |item|
+      item.each do |k,v|
+        output << k if v ==k
+      end
+    end
+
+    output
   end
 end
